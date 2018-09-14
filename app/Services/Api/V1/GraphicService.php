4a1e93e6abff03f92f6981ee58fe2854aaec5012
Weslley Ribeiro <weslley.ribeiro@asenses.com.br>
2018-09-13 23:09:50 -0300

    formatando dados dos dash

--- app/Services/Api/V1/GraphicService.php
+++ app/Services/Api/V1/GraphicService.php
@@ -58,16 +58,17 @@ class GraphicService
 
         ];
 
-        $universe['postsPercent']    = number_format(($universe['sumPostsHashtag']*100)/$universe['sumPostsToday'], 1)."%";
-        $universe['commentsPercent'] = number_format(($universe['sumCommentsHashtag']*100)/$universe['sumCommentsToday'], 1)."%";
-        $universe["likesPercent"]    = number_format(($universe['sumLikesHashtag']*100)/$universe['sumLikesToday'], 1)."%";
-        $universe["universePercent"] = number_format(($universe['sumPostsHashtag']*100)/$universe["universeHashtag"], 1)."%";
+        $universe['postsPercent']    = number_format(($universe['sumPostsHashtag']*100)/$universe['sumPostsToday'], 2,',','.')."%";
+        $universe['commentsPercent'] = number_format(($universe['sumCommentsHashtag']*100)/$universe['sumCommentsToday'], 2,',','.')."%";
+        $universe["likesPercent"]    = number_format(($universe['sumLikesHashtag']*100)/$universe['sumLikesToday'], 2,',','.')."%";
+        $universe["universePercent"] = number_format(($universe['sumPostsHashtag']*100)/$universe["universeHashtag"], 2,',','.')."%";
         $universe['totalFollowers']  = collect();
 
         foreach ($this->_influencerModel->all() as $influencer){
             $universe['totalFollowers']->push($influencer->profile->followed_by);
         }
         $universe['totalFollowers']  = $universe['totalFollowers']->sum();
+        //$universe['totalFollowers']  = number_format((int)$universe['totalFollowers']->sum(), 2,',','.');
         $universe['engagement']      = round(($universe['sumLikesHashtag'] + $universe['sumCommentsHashtag'])/$universe['totalFollowers'], 2)."%";
         $universe['impact']          = round((($universe['sumPostsHashtag'] * $universe['totalFollowers']) * 4)/100,0 );
 
