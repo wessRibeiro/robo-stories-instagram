269651ff5a180038eb28de3733144103d7a57df6
Guilherme <guilherme@Developers-iMac.local>
2018-10-09 17:27:56 -0300

    Recebimento de id de influencer de destino de historias

--- app/Services/Api/V1/StoryService.php
+++ app/Services/Api/V1/StoryService.php
@@ -3,11 +3,10 @@
  * Created by PhpStorm.
  * User: User
  * Date: 05/09/2018
- * Time: 13:53
+ * Time: 13:53.
  */
 
 namespace Louder\Services\Api\V1;
-
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
@@ -26,99 +25,100 @@ class StoryService
                                 Story           $storyModel,
                                 Request         $request)
     {
-        $this->_route               = $route;
-        $this->_request             = $request;
-        $this->_influencerModel     = $influencerModel->setConnection($this->_route->parameter('program'));
-        $this->_storyModel          = $storyModel->setConnection($this->_route->parameter('program'));
+        $this->_route = $route;
+        $this->_request = $request;
+        $this->_influencerModel = $influencerModel->setConnection($this->_route->parameter('program'));
+        $this->_storyModel = $storyModel->setConnection($this->_route->parameter('program'));
     }
 
-    public function index(){
+    public function index()
+    {
         $dataInfluencerHasStories = [];
 
         foreach ($this->_influencerModel->where('ativo', true)->get() as $influencer) {
-            foreach($influencer->stories()->where([
-                                                    ['temhashtag', '=', true],
-                                                    ['aprovado',   '=', false]
-                                                  ])->get()
+            foreach ($influencer->stories()->where([
+                                                ['temhashtag', '=', true],
+                                                ['aprovado',   '=', false],
+                                            ])->get()
                     as $key => $story
-            ){
-
+            ) {
                 array_push($dataInfluencerHasStories,
-                           [
-                               'name'              => trim($influencer->nome),
-                               'instagramUser'     => $influencer->instagram,
-                               'pictureProfile'    => $influencer->img,
-                                'urlStory'         => $story->urlimg,
-                                'datePost'         => mysql_br_date_time($story->vinculadoem),
-                                'midiaType'        => $story->midia_type,
-                                'instagramStoryId' => $story->instagram_story_id,
-                                'is_geral'         => $influencer->is_geral,
-                           ]
+                    [
+                        'name' => trim($influencer->nome),
+                        'instagramUser' => $influencer->instagram,
+                        'pictureProfile' => $influencer->img,
+                        'urlStory' => $story->urlimg,
+                        'datePost' => mysql_br_date_time($story->vinculadoem),
+                        'midiaType' => $story->midia_type,
+                        'instagramStoryId' => $story->instagram_story_id,
+                        'is_geral' => $influencer->is_geral,
+                    ]
                 );
-
             }
         }
 
         return $dataInfluencerHasStories;
     }
 
-    public function getInfluencers(){
+    public function getInfluencers()
+    {
         return $this->_influencerModel->where('ativo', '=', 1)->get();
     }
 
     public function approve($instagramStoryId)
     {
-        try{
-            if ($this->_storyModel->where('instagram_story_id', '=', $instagramStoryId)
-                                  ->update([
-                                        'aprovado'      => true,
-                                        'justificativa' => NULL,
-                                  ])
-            ){
+        try {
+            $update = array();
+            $update['aprovado'] = true;
+            $update['justificativa'] = null;
+
+            if ($this->_request->get('influencerId') != null) {
+                $update['iduser'] = $this->_request->get('influencerId');
+            }
+
+            if ($this->_storyModel->where('instagram_story_id', '=', $instagramStoryId)->update($update)) {
                 return [
-                    'code'      => 200,
-                    'message'   => 'Story aprovado!'
+                    'code' => 200,
+                    'message' => 'Story aprovado!',
                 ];
-            }else{
+            } else {
                 return [
-                    'code'      => 404,
-                    'message'   => 'Erro ao tentar aprovar!'
+                    'code' => 404,
+                    'message' => 'Erro ao tentar aprovar!',
                 ];
             }
-        }catch (\Exception $ex){
+        } catch (\Exception $ex) {
             return [
                 'code' => 400,
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
-
         }
     }
 
     public function disapprove($instagramStoryId)
     {
-        try{
+        try {
             if ($this->_storyModel->where('instagram_story_id', '=', $instagramStoryId)
                                   ->update([
-                                                'aprovado'      => 2,
-                                                'justificativa' => $this->_request->get('justification'),
-                                           ])
+                                        'aprovado' => 2,
+                                        'justificativa' => $this->_request->get('justification'),
+                                    ])
             ) {
                 return [
-                    'code'      => 200,
-                    'message'   => 'Story reprovado!'
+                    'code' => 200,
+                    'message' => 'Story reprovado!',
                 ];
-            }else{
+            } else {
                 return [
-                    'code'      => 404,
-                    'message'   => 'Erro ao tentar reprovar!'
+                    'code' => 404,
+                    'message' => 'Erro ao tentar reprovar!',
                 ];
             }
-        }catch (\Exception $ex){
+        } catch (\Exception $ex) {
             return [
                 'code' => 400,
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
-
         }
     }
 }
