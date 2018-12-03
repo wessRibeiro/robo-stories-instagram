16ef57855a6230959fa9c540e6b88c060cdf03b5
Guilherme <guilherme@Developers-iMac.local>
2018-12-03 14:29:33 -0200

    Dashboard MSL

--- app/Services/Api/V1/GraphicService.php
+++ app/Services/Api/V1/GraphicService.php
@@ -59,8 +59,8 @@ class GraphicService
             'sumLikesHashtag'           => collect($this->_analyticsModel->pluck('likesHashtag'))->sum(),
             'sumCommentsHashtag'        => collect($this->_analyticsModel->pluck('comentariosHashtag'))->sum(),
             'universeHashtag'           => collect($this->_analyticsModel->pluck('universoHashtag'))->pop(),
-            'sumPostsHashtag'           => collect($this->_postsCuradoriaModel->where('aprovado', '=', 1)->get())->count(),
-            'sumPostsCuradoria'         => collect($this->_postsCuradoriaModel->select('*')->get())->count(),
+            'sumPostsHashtag'           => collect($this->_postsCuradoriaModel->where('aprovado', '=', 1))->count(),
+            'sumPostsCuradoria'         => collect($this->_postsCuradoriaModel->select('*'))->count(),
         ];
 
         $universe['postsPercent']    = number_format(($universe['sumPostsCuradoria']*100)/$universe['sumPostsToday'], 2,',','.')."%";
